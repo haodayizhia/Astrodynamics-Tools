4888cf625977251fe99aa1eb4449f021d8012169
ltr-pc <50456094+haodayizhia@users.noreply.github.com>
2024-06-17 12:48:02 +0800

    rv_elements

--- rv_elements/rv_elements.h
+++ rv_elements/rv_elements.h
@@ -1,9 +1,10 @@
-// 绕地rv转换为轨道根数函数
+// 绕地rv转换为轨道根数
 #include <vector>
+#include <math.h>
 double mu = 3.986005e14;
-double norm(double x, double y, double z)
+double norm(std::vector<double> vec)
 {
-    return sqrt(x * x + y * y + z * z);
+    return sqrt(pow(vec.at(0), 2) + pow(vec.at(1), 2) + pow(vec.at(2), 2));
 }
 std::vector<double> cross(std::vector<double> v1, std::vector<double> v2)
 {
@@ -11,12 +12,26 @@ std::vector<double> cross(std::vector<double> v1, std::vector<double> v2)
                                v1.at(2) * v2.at(0) - v1.at(0) * v2.at(2),
                                v1.at(0) * v2.at(1) - v1.at(1) * v2.at(0)};
 }
+double dot(std::vector<double> v1, std::vector<double> v2)
+{
+    return v1.at(1) * v2.at(1) + v1.at(2) * v2.at(2) + v1.at(3) * v2.at(3);
+}
 std::vector<double> rv2eles(std::vector<double> rv)
 {
     std::vector<double> eles(6);
-    double r_norm = norm(rv.at(0), rv.at(1), rv.at(2));
-    double v_norm = norm(rv.at(3), rv.at(4), rv.at(5));
-    // Apogee:km
+    std::vector<double> r(rv.begin(), rv.begin() + 2);
+    double r_norm = norm(r);
+    std::vector<double> v(rv.begin() + 3, rv.end());
+    double v_norm = norm(v);
+    std::vector<double> h = cross(r, v);
+    double h_norm = norm(h);
+    std::vector<double> Omega = cross(std::vector<double>{0, 0, 1}, h);
+    // Apogee(km)
     eles.at(0) = -mu / 2 * (pow(v_norm, 2) / 2 - mu / r_norm);
-    eles.at(1) = 
+    // Eccentricity
+    eles.at(1) = sqrt(1 - pow(h_norm, 2) / mu / eles.at(0));
+    // Inclination(rad)
+    eles.at(2) = acos(dot(std::vector<double>{0, 0, 1}, h) / h_norm);
+    // Right Ascension of Ascending Node(rad)
+    eles.at(3) = acos
 }
--- test.cpp
+++ test.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
+#include <math.h>
 int main(int argc, char *argv[])
 {
-    a=
+    std::cout << acos(0.5)/3.14*180 << std::endl;
+    return 0;
 }
