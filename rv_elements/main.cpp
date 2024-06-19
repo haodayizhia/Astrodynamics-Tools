fa2f307d9f152c9beb7508a1a0bbe2e4e060c694
ltr-pc <50456094+haodayizhia@users.noreply.github.com>
2024-06-19 12:52:52 +0800

    123

--- rv_elements/main.cpp
+++ rv_elements/main.cpp
@@ -6,13 +6,14 @@
 int main(int argc, char *argv[])
 {
     std::ifstream file("MEME_799500932_STARLINK-31920_1520146_Operational_1401414420_UNCLASSIFIED.txt");
+    std::ofstream ofile("1.txt");
     std::string line;
     for (int i = 0; i < 4; ++i)
     {
         std::getline(file, line);
     }
     std::vector<std::vector<double>> date_rv;
-    std::vector<std::vector<double>> date_eles;
+    // std::vector<std::vector<double>> date_eles;
     int lineNumber = 0;
     int lineOffset = 4;
     while (std::getline(file, line))
@@ -30,15 +31,13 @@ int main(int argc, char *argv[])
     }
     for (auto iterator = date_rv.cbegin(); iterator != date_rv.cend(); ++iterator)
     {
-        std::vector<double> row = {(*iterator).front()};
+        // std::vector<double> row = {(*iterator).front()};
         std::vector<double> eles = rv2eles(std::vector<double>((*iterator).begin() + 1, (*iterator).end()));
-        row.insert(row.end(), eles.begin(), eles.end());
-        date_eles.push_back(row);
-    }
-    for (auto i : date_eles)
-    {
-        for (auto j : i)
-            std::cout << j << ' ';
-        std::cout << std::endl;
+        fprintf(ofile, "%.3f", (*iterator).front());
+        for (auto i : eles)
+            ofile << ' ' << i;
+        ofile << std::endl;
+        // row.insert(row.end(), eles.begin(), eles.end());
+        // date_eles.push_back(row);
     }
 }
